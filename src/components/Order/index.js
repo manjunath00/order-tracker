d29d2e30d7c815ac3873b2bfcd332e292ff25ccd
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-07 05:01:30 +0530

    added all routes related component to one file

---
+++ src/components/Order/index.js
@@ -0,0 +1,13 @@
+import OrderDelete from "./OrderDelete";
+import OrderEdit from "./OrderEdit";
+import OrderNew from "./OrderNew";
+import OrdersList from "./OrdersList";
+
+
+export {
+    OrderDelete,
+    OrderEdit,
+    OrderNew,
+    OrdersList    
+};
+
