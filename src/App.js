6c3da47523881fb95cc71ef08e9f4df8c05c508f
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-07 04:58:27 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+
+import Header from "./components/Header";
+import {
+  OrderDelete,
+  OrderEdit,
+  OrderNew,
+  OrdersList    
+} from "./components/Order/index.js";
+// import OrdersList from "./components/OrdersList";
+// import OrderNew from "./components/OrderNew";
+// import OrderEdit from "./components/OrderEdit";
+// import OrderDelete from "./components/OrderDelete";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="mx-auto">
+        <Header />
+        <OrdersList />
+        <Switch>
+          {/* <Route exact={true} path="/orders/orders" component={OrdersList} /> */}
+          <Route exact={true} path="/orders/new" component={OrderNew} />
+          <Route exact={true} path="/orders/edit/:id" component={OrderEdit} />
+          <Route
+            exact={true}
+            path="/orders/delete/:id"
+            component={OrderDelete}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
