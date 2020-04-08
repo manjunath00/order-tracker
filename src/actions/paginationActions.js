ad49d2922714eb44b5c2fe92409052e04de63f3b
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-09 01:47:41 +0530

    actions prev page and next pages completed

---
+++ src/actions/paginationActions.js
@@ -0,0 +1,30 @@
+import {NEXT_PAGE} from "./types";
+import {PREV_PAGE} from "./types";
+import {SET_ITEMS_PER_PAGE} from "./types";
+
+export const nextPage = (value, totalPages = 11111) => {
+  if (value < totalPages) {
+    value++;
+  }
+  return {
+    type: NEXT_PAGE,
+    payload: value,
+  };
+};
+
+export const prevPage = (value) => {
+  if (value > 1) {
+    value--;
+  }
+  return {
+    type: PREV_PAGE,
+    payload: value,
+  };
+};
+
+export const setItemsPerPage = (value) => {
+  return {
+    type: SET_ITEMS_PER_PAGE,
+    payload: value,
+  };
+};
