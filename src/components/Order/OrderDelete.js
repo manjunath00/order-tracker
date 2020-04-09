1e7a29d46f8c178c33429e62d61778cbdc98c515
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-09 12:34:33 +0530

    Delete order completed

--- src/components/Order/OrderDelete.js
+++ src/components/Order/OrderDelete.js
@@ -1,17 +1,30 @@
 import React from "react";
+import {connect} from "react-redux";
 import Modal from "../common/Modal";
+import {deleteOrder} from "../../actions/ordersActions";
+import history from "../../history";
 
-function OrderDelete() {
+function OrderDelete(props) {
+  const goBack = () => {
+    history.push("/")
+  };
+
+  const deleteOrder = () => {
+    const id = props.match.params.id;
+    props.deleteOrder(id);
+    goBack();
+  };
+
   return (
-    <Modal>
+    <Modal onDismiss={() => history.push("/")}>
       <div>
         <div>Are you sure you want to delete this order ?</div>
         <div>
           <div>
-            <button>Delete</button>
+            <button onClick={() => deleteOrder()}>Delete</button>
           </div>
           <div>
-            <button>Cancel</button>
+            <button onClick={() => goBack()}>Cancel</button>
           </div>
         </div>
       </div>
@@ -19,4 +32,4 @@ function OrderDelete() {
   );
 }
 
-export default OrderDelete;
+export default connect(null, {deleteOrder})(OrderDelete);
