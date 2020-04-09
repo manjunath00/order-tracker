770244ff86c973a2cee486c31aa7cbc4ecb335f9
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-09 12:47:19 +0530

    completed modal for edit delete, createnew order

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../../history";
 
 const Modal = (props) => {
+  console.log(props)
   return ReactDOM.createPortal(
-    <div className="modal">
-      <div className="modal-body">{props.children}</div>
+    // <div className="modal" onClick={props.onDismiss}>
+    <div className="modal" onClick={() => history.push('/')}>
+      <div className="modal-body" onClick={(e) => e.stopPropagation() }>{props.children}</div>
     </div>,
     document.querySelector("#modal")
   );
 };
 
 export default Modal;
+// export default withRouter(Modal);
