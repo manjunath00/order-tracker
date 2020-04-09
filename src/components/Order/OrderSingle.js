7ff9df6087a1b134e1523cac65064e38523bb57a
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-09 12:33:35 +0530

    single order functionality completed

--- src/components/Order/OrderSingle.js
+++ src/components/Order/OrderSingle.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function OrderSingle({order}) {
+function OrderSingle({order}) { 
   return (
     <div className="row order" id={order.id}>
       <div>{order.customer_name}</div>
@@ -9,10 +9,14 @@ function OrderSingle({order}) {
       <div>{order.product}</div>
       <div className="order-quantity">{order.quantity}</div>
       <div>
-        <Link to={`/orders/edit/${order.id}`}>Edit</Link>
+        <Link to={{pathname: `/orders/edit/${order.id}`}}>
+          Edit
+        </Link>
       </div>
       <div>
-        <Link to={`/orders/delete/${order.id}`}>Delete</Link>
+        <Link to={{ pathname:`/orders/delete/${order.id}`, state: {order: order}}}>
+          Delete
+        </Link>
       </div>
     </div>
   );
