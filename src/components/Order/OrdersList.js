d2bcb48c5c26ae4a86a3286b986aa5c2c2fa6e76
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-09 12:38:12 +0530

    refactored & connected to redux store.

--- src/components/Order/OrdersList.js
+++ src/components/Order/OrdersList.js
@@ -1,51 +1,28 @@
-import React, {useState } from "react";
-import data from "../../DummyData.json";
+import React from "react";
+import {connect} from "react-redux";
 import Pagination from "../common/Pagination";
-
 import OrdersListHeader from "./OrdersListHeader";
 import OrderSingle from "./OrderSingle";
 import OrderHeader from "./OrderHeader";
 
-function OrdersList() {
-  const [orders, setOrders] = useState(data);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [ordersPerPage, setOrdersPerPage] = useState(5);
-  const [sortState, setSortState] = useState(true); 
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  const changeOrdersPerPage = (noOfOrders) => setOrdersPerPage(noOfOrders);
-
-  const indexOfLastPost = currentPage * ordersPerPage;
-  const indexOfFirstPost = indexOfLastPost - ordersPerPage;
-  const currentOrders = orders.slice(indexOfFirstPost, indexOfLastPost);
-  const totalPages = Math.ceil(orders.length / ordersPerPage);
-
-  const onSort = (field) => {
-    console.log("Trying to sort");
-    setSortState(!sortState);
-    const multiply = sortState ? 1 : -1;
-    const sortedOrder = [...orders].sort(
-      (a, b) => a[field] * multiply + -1 * multiply * b[field]
-    );
-    setOrders(sortedOrder);
-  };
-
+function OrdersList(props) {
+  const arrayStart = props.pages.arrayStartIndex;
+  const arrayEnd = props.pages.arrayEndIndex;
   return (
     <div className="order-list">
       <OrderHeader />
-      <OrdersListHeader onSort={onSort} />
-      {currentOrders.map((item, index) => (
+      <OrdersListHeader />
+      {props.orders.slice(arrayStart, arrayEnd).map((item, index) => (
         <OrderSingle order={item} key={index} />
       ))}
 
-      <Pagination
-        ordersPerPage={ordersPerPage}
-        currentPage={currentPage}
-        paginate={paginate}
-        changeOrderPerPage={changeOrdersPerPage}
-      />
+      <Pagination totalItems={props.orders.length} />
     </div>
   );
 }
 
-export default OrdersList;
+const mapStateToProps = (state) => {
+  return {orders: state.orders, pages: state.pages};
+};
+
+export default connect(mapStateToProps, {})(OrdersList);
