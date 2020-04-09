8bef30b7e101a3151c1351856fdac2b8392cb9a9
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-09 12:39:09 +0530

    connected to redux store for sort functionality

--- src/components/Order/OrdersListHeader.js
+++ src/components/Order/OrdersListHeader.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import {connect} from "react-redux";
+import {sortOrder} from "../../actions/ordersActions";
 
-function OrdersListHeader({onSort}) {
+function OrdersListHeader({sortOrder}) {
+  const [sortDirection, setsortDirection] = useState(true);
+
+  const onClickForSort = (field) => {
+    sortOrder(field, sortDirection);
+    setsortDirection(!sortDirection);
+  };
+
+  // <button className="headerSortUp"></button>
   return (
     <div className="row order-header">
-      <div>customer_name</div>
-      <div>customer_email</div>
-      <div>Product</div>
-      <div className="order-quantity">
-        <button onClick={() => onSort("quantity")}>Quantity</button>
+      <div
+        className="headerSortUp"
+        onClick={() => onClickForSort("customer_name")}
+      >
+        customer_name
+      </div>
+      <div
+        className="headerSortUp"
+        onClick={() => onClickForSort("customer_email")}
+      >
+        customer_email
+      </div>
+      <div className="headerSortUp" onClick={() => onClickForSort("product")}>
+        Product
+      </div>
+      <div onClick={() => onClickForSort("quantity")} className="headerSortUp">
+        Quantity
       </div>
       <div></div>
     </div>
   );
 }
 
-export default OrdersListHeader;
+export default connect(null, {sortOrder})(OrdersListHeader);
